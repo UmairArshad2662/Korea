329e3689ba0f5598b8aad1feac4e0818c6973814
AyzennTech <168638793+AyzennTech@users.noreply.github.com>
2024-09-08 20:18:29 +0500

    Formated

--- backend/Models/Merchant.js
+++ backend/Models/Merchant.js
@@ -1,29 +1,29 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const MerchantSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,  // Ensure unique email for each merchant
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    businessName: {
-        type: String,
-        required: true,
-    },
-    address: {
-        type: String,
-        required: true,
-    }
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true, // Ensure unique email for each merchant
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  businessName: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
 });
 
-const MerchantModel = mongoose.model('merchants', MerchantSchema);
+const MerchantModel = mongoose.model("merchants", MerchantSchema);
 module.exports = MerchantModel;
--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,19 @@
-const express = require ('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const cors = require ('cors');
-const AuthRouter = require ('./Routes/AuthRouter')
-const MerchantRouter = require('./Routes/MerchantRouter'); // Add MerchantRouter
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const AuthRouter = require("./Routes/AuthRouter");
+const MerchantRouter = require("./Routes/MerchantRouter"); // Add MerchantRouter
 
-
-require ('dotenv').config();
-require('./Models/db');
+require("dotenv").config();
+require("./Models/db");
 const PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use('/auth', AuthRouter);
-app.use('/merchant', MerchantRouter);  // Add Merchant routes
+app.use("/auth", AuthRouter);
+app.use("/merchant", MerchantRouter); // Add Merchant routes
 
-app.listen(PORT,()=>{
-    console.log(`server is running on ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`server is running on ${PORT}`);
+});
