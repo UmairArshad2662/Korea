3b39d9df76d82fb061a79c43cbe1df0747130c5e
AyzennTech <168638793+AyzennTech@users.noreply.github.com>
2024-09-08 19:37:24 +0500

    Signup/Login for merchant

---
+++ backend/Models/Merchant.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const MerchantSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,  // Ensure unique email for each merchant
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    businessName: {
+        type: String,
+        required: true,
+    }
+});
+
+const MerchantModel = mongoose.model('merchants', MerchantSchema);
+module.exports = MerchantModel;
